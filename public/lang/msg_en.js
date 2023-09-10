e687d9c599186b5ceb61c8ba3b7ec8ef85da56f9
peko0o <144571476+peko0o@users.noreply.github.com>
2023-09-10 00:17:05 -0300

    Update msg_en.js

--- public/lang/msg_en.js
+++ public/lang/msg_en.js
@@ -1,3 +1,3 @@
 var MSG_GIFT = "obrigado |username| pelo presente";
-var MSG_WINNER = "parabés a  |username| pelo presente";
+var MSG_WINNER = "parabéns a  |username| pelo presente";
 var MSG_TEST = "Test sound";
